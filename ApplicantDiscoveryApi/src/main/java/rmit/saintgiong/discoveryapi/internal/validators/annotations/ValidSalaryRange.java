10068f8844268152564ee8e30054cca7aa97f969
NgocNguyen <s3924436@rmit.edu.vn>
2025-12-17 14:58:14 +0700

    refractor: remove unused import

--- ApplicantDiscoveryApi/src/main/java/rmit/saintgiong/discoveryapi/internal/validators/annotations/ValidSalaryRange.java
+++ ApplicantDiscoveryApi/src/main/java/rmit/saintgiong/discoveryapi/internal/validators/annotations/ValidSalaryRange.java
@@ -4,7 +4,12 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import rmit.saintgiong.discoveryapi.internal.validators.constraints.SalaryRangeValidator;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 
 /**
  * Validates that salaryMin is less than or equal to salaryMax when both are provided.
