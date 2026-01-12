9ff579668c9e0849d766506e860c92f3dbca7265
NgocNguyen <s3924436@rmit.edu.vn>
2026-01-13 02:48:38 +0700

    feat: resolve comment

--- ApplicantDiscoveryApi/src/main/java/rmit/saintgiong/discoveryapi/internal/common/types/type/KafkaTopic.java
+++ ApplicantDiscoveryApi/src/main/java/rmit/saintgiong/discoveryapi/internal/common/types/type/KafkaTopic.java
@@ -4,13 +4,10 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public final class KafkaTopic {
-    public static final String ADD_APPLICANT_TOPIC_REQUEST = "JA_APPLICANT_CREATED";
-    public static final String ADD_APPLICANT_TOPIC_REPLIED = "JA_ADD_APPLICANT_REPLIED";
+    public static final String  JA_APPLICANT_CREATED_TOPIC = "JA_APPLICANT_CREATED";
 
-    public static final String UPDATE_APPLICANT_TOPIC_REQUEST = "JA_APPLICANT_UPDATED";
-    public static final String UPDATE_APPLICANT_TOPIC_REPLIED = "JA_UPDATE_APPLICANT_REPLIED";
+    public static final String JA_APPLICANT_UPDATED_TOPIC= "JA_APPLICANT_UPDATED";
 
-    public static final String DELETE_APPLICANT_TOPIC_REQUEST = "JA_APPLICANT_DELETED";
-    public static final String DELETE_APPLICANT_TOPIC_REPLIED = "JA_DELETE_APPLICANT_REPLIED";
+    public static final String JA_APPLICANT_DELETED_TOPIC  = "JA_APPLICANT_DELETED";
 
 }
